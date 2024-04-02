37c2b2ddea33f95cd643e1b86dd36793b7de69e4
babubhiaraju <165696875+babubhiaraju@users.noreply.github.com>
2024-04-02 21:38:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923446671982', 'ADNAN WARIS', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
